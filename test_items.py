6171747f6db9717e8bd5edc6aefdf4ad817a3508
Алексей Спирин <aspirin160691@gmail.com>
2020-02-02 15:29:39 +0300

    lesson 3.6 pytest? last step

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_add(browser):
+    browser.get(link)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed(), "Кнопки добавить на странице нет"
+    time.sleep(30)
